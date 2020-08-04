b4994215ecb811a268323aa2b1b97a0706148374
BenFeder <BenjaminTFeder@gmail.com>
2020-08-04 16:44:27 -0400

    Variables exercise completed.

--- code.js
+++ code.js
@@ -15,34 +15,46 @@ console.log(4 + 5);
 
 // 1.) Log an expression that evaluates multiplying two integers.
 // your code here
-console.log("answer 1");
+let answer1 = 4 * 3;
+console.log(answer1);
 
 // 2.) log an expression that evaluates to the difference of two integers (subtracting one from the other)
 // your code here
-console.log("answer 2");
+let answer2 = 12 - 9;
+console.log(answer2);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here
-console.log("answer 3");
+let answer3 = "I'm writing" + " " + "my first JavaScript code";
+console.log(answer3);
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here
-console.log("answer 4");
+let x = 42;
+let y = 21;
+console.log(x > y);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log("answer 5");
+let answer5 = 25 % 7;
+console.log(answer5);
 
 // 6.) Create a variable and name it "sentence".  Assign a string containing a sentence of text to it.
 // Then log the variable.
-
 // Your code here
-console.log("answer 6");
+let sentence = "My favorite color is orange.";
+console.log(sentence);
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-console.log("answer 7");
+let str1 = "I'm learning ";
+let str2 = "a bit ";
+let str3 = "of JavaScript ";
+let str4 = "at Kenzie";
+
+let answer7 = str1 + str2 + str3 + str4;
+console.log(answer7);
 
 // 8.) Add parenthesis to the following statement, the result should be 1
 // Hint: You will need three sets of parenthesis
 
-console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
+console.log((3 * 3 + 1) / (20 / 2) == 1); // This should log true
